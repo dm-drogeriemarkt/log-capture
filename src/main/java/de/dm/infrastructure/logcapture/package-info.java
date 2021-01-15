815a932622f9ba2ccc47f4319e2a44747a52a57b
Daniel Flassak <daniel.flassak@dm.de>
2021-01-15 13:46:49 +0100

    add missing Javadoc to package-info.java

--- src/main/java/de/dm/infrastructure/logcapture/package-info.java
+++ src/main/java/de/dm/infrastructure/logcapture/package-info.java
@@ -1,1 +1,5 @@
+/**
+ * Everything regarding LogCapture is here
+ * See README.md or https://github.com/dm-drogeriemarkt/structured-logging for more information on usage
+ */
 package de.dm.infrastructure.logcapture;
