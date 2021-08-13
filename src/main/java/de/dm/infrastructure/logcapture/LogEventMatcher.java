577e60081f9994e07bd70de10d4ab26ee2c86f8f
Daniel Flassak <d0a03856@dm.de>
2021-08-13 16:21:53 +0200

    add javadoc for LogEventMatcher

--- src/main/java/de/dm/infrastructure/logcapture/LogEventMatcher.java
+++ src/main/java/de/dm/infrastructure/logcapture/LogEventMatcher.java
@@ -1,9 +1,31 @@
 package de.dm.infrastructure.logcapture;
 
+/**
+ * interface for matching captured log events
+ */
 public interface LogEventMatcher {
+    /**
+     * should return true if the logged event is matched
+     *
+     * @param loggedEvent logged event to check for match
+     *
+     * @return true if the event is matched
+     */
     boolean matches(LoggedEvent loggedEvent);
 
+    /**
+     * returns an error message describing why a logged event does not match
+     *
+     * @param loggedEvent logged event that does not match
+     *
+     * @return description why the event does not match
+     */
     String getNonMatchingErrorMessage(LoggedEvent loggedEvent);
 
+    /**
+     * short description of what aspect of the log event matcher matches, for example "marker name" or "Exception"
+     *
+     * @return matched acpect
+     */
     String getMatcherDescription();
 }
