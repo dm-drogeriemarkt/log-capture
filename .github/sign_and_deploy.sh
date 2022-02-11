0916f77824c99e8df6d36e2896c48f324823d6b8
Daniel Flassak <daniel.flassak@dm.de>
2022-02-11 17:51:53 +0100

    set version for deployment

--- .github/sign_and_deploy.sh
+++ .github/sign_and_deploy.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
-echo $GPG_KEY_BASE64 | base64 -d  > daniel.flassak.open.source.private.key
-gpg --passphrase "${GPG_PASSPHRASE}" --batch --yes --fast-import daniel.flassak.open.source.private.key
+KEY_FILE='daniel.flassak.open.source.private.key'
 
-# TODO: only deploy for tags on master
-# TODO: where does the version come from here?
-# TODO: maven batch mode everywhere
-echo gpg keyname $GPG_KEYNAME
+echo $GPG_KEY_BASE64 | base64 -d  > ${KEY_FILE}
+gpg --passphrase "${GPG_PASSPHRASE}" --batch --yes --fast-import ${KEY_FILE}
 
-mvn --batch-mode install -P sign --settings .github/mvnsettings.xml -DskipTests=true
+echo gpg keyname ${GPG_KEYNAME}
 
-tree target
+if [[ "${REF_TYPE}" == "tag" ]]; then
+    mvn --batch-mode install -P sign -DskipTests=true -Dproject.version=${REF_NAME}
+    SUCCESS=$?
+else
+    echo "this should only be run for tags"
+    SUCCESS=1
+fi
 
-# just to be safe, although this should not be necessary
-rm daniel.flassak.open.source.private.key
+tree target # TODO: remove
+
+# just to be safe, although this deleting these should not be necessary
+rm ${KEY_FILE}
 rm -rf ~/.gnupg
+
+exit ${SUCCESS}
