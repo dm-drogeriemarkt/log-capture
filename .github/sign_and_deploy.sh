67538e9ebf95a0c00ccde05261affd79393db439
Daniel Flassak <daniel.flassak@dm.de>
2022-02-11 18:48:38 +0100

    find out why signing does not work in the pipeline (gpg:sign seems to be ignored)

--- .github/sign_and_deploy.sh
+++ .github/sign_and_deploy.sh
@@ -7,7 +7,7 @@ gpg --passphrase "${GPG_PASSPHRASE}" --batch --yes --fast-import ${KEY_FILE}
 echo gpg keyname ${GPG_KEYNAME}
 
 if [[ "${REF_TYPE}" == "tag" ]]; then
-    mvn --batch-mode -DskipTests=true -Dproject.version=${REF_NAME} package gpg:sign deploy
+    mvn --batch-mode -DskipTests=true -Dproject.version=${REF_NAME} clean package gpg:sign && tree target && mvn --batch-mode -DskipTests=true -Dproject.version=${REF_NAME} deploy
     SUCCESS=$?
 else
     echo "this should only be run for tags"
