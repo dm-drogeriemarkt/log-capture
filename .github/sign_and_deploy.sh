7f480d74e634f301d41dcca29557bfbd59881cee
Daniel Flassak <daniel.flassak@dm.de>
2022-02-14 08:33:39 +0100

    update comment and remove 'tree' used for debugging

--- .github/sign_and_deploy.sh
+++ .github/sign_and_deploy.sh
@@ -7,18 +7,23 @@ gpg --passphrase "${GPG_PASSPHRASE}" --batch --yes --fast-import ${KEY_FILE}
 echo gpg keyname ${GPG_KEYNAME}
 
 if [[ "${REF_TYPE}" == "tag" ]]; then
-    # -P sign plugin is used instead of gpg:sign because gpg:sign has side-effects
-    # also, install cannot be used because deploy will cause the signatures to be invalid because it
-    # re-creates the jars. So explicitly calling source:jar and javadoc:jar seems to be the only
-    # viable solution.
+    # 'install' cannot be used in addition to 'deploy', because that makes the signatures invalid by re-creating jars
+    # after they have been signed.
+    #
+    # So we can **only** call the 'deploy' target here, which is why 'source:jar' and 'javadoc:jar' are called
+    # explicitly before 'deploy' so that their artifacts are signed, too.
+    #
+    # '-P sign' is used here instead of 'gpg:sign', because 'gpg:sign' seemingly has the same effect as 'install'
+    # (invalid signatures to to re-created jars)
+    #
+    # There may be an easier way to sign and deploy all the artifacts (sources, javadoc, binaries and pom), but after
+    # four hours of debugging this, I'm satisfied that it works at all.
     mvn --batch-mode -DskipTests=true -Dproject.version=${REF_NAME} -P sign clean source:jar javadoc:jar deploy
     SUCCESS=$?
 else
     echo "this should only be run for tags"
     SUCCESS=1
 fi
-
-tree target # TODO: remove
 
 # just to be safe, although this deleting these should not be necessary
 rm ${KEY_FILE}
