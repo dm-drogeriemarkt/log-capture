06dcca48451206ba9ca487c36a4b23dd9a865455
Daniel Flassak <daniel.flassak@dm.de>
2025-01-10 15:11:01 +0100

    update dependencies

---
+++ do-dependency-updates.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# set -x # set this for debugging to print all called commands
+set -e # fail non non-zero exit status
+set -u # fail on unset variable
+set -o pipefail # fail if a pipe fails
+
+cd "$(dirname '${0}')"
+
+mvn versions:use-latest-releases
